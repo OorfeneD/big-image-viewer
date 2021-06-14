40ade30f5cbb34c0436e1c11ec351c83b0d628c8
OorfeneDeuce <oorfened@gmail.com>
2021-06-14 08:33:36 +0000

    fixes: minor changes to index.js minor update of createPyramid and createTiles functionality minor update of createPyramid - resolving pyramid filenames createPyramid and createTiles now didn`t create extra folder to output

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const createTiles = async (fileName, outFolder, tileSize = 512) => {
   if (!fileName || !outFolder) throw new Error('Parameters must be not empty')
   if (typeof fileName !== 'string' || typeof outFolder !== 'string') throw new Error('Parameters must be are string')
   if (!existsSync(fileName)) throw new Error('Image file not found')
+  const imageName = basename(fileName, extname(fileName))
   const image = sharp(fileName, {
     limitInputPixels: false,
     pages: -1
   })
-  const tilesOutFolder = join(outFolder, basename(fileName, extname(fileName)))
+  const tilesOutFolder = join(outFolder)
   try {
     if (!existsSync(tilesOutFolder)) {
       mkdirSync(tilesOutFolder, {
@@ -27,17 +28,19 @@ const createTiles = async (fileName, outFolder, tileSize = 512) => {
     for (let i = 0; i <= Math.floor(meta.width / tileSize); i++) {
       for (let j = 0; j <= Math.floor(meta.height / tileSize); j++) {
         promises.push(new Promise(function (resolve, reject) {
-          image.extract({
+          const options = {
             left: i * tileSize,
             top: j * tileSize,
             width: (i + 1) * tileSize < meta.width ? tileSize : meta.width - i * tileSize,
             height: (j + 1) * tileSize < meta.height ? tileSize : meta.height - j * tileSize
-          })
+          }
+          if (!(!options.width || !options.height)) {
+            image.extract(options)
             .jpeg({
               quality: 80
             })
             .withMetadata()
-            .toFile(join(tilesOutFolder, `${i}_${j}.jpg`), err => {
+              .toFile(join(tilesOutFolder, `${imageName}_${i}_${j}.jpg`), err => {
               if (err) {
                 reject(err)
               }
@@ -46,6 +49,9 @@ const createTiles = async (fileName, outFolder, tileSize = 512) => {
               }
               resolve()
             })
+          } else {
+            resolve()
+          }
         }))
       }
     }
@@ -58,12 +64,13 @@ const createPyramid = async (fileName, outFolder = '') => {
   if (typeof fileName !== 'string' || typeof outFolder !== 'string') throw new Error('Parameters must be are string')
   if (!existsSync(fileName)) throw new Error('Image file not found')
   const scales = [1024, 2048, 4096, 8192, 16384, 32768, 65536]
+  const imageName = basename(fileName, extname(fileName))
   const image = sharp(fileName, {
     limitInputPixels: false,
     pages: -1
   })
   if (outFolder === '') outFolder = dirname(fileName)
-  const pyramidOutFolder = join(outFolder, basename(fileName, extname(fileName)))
+  const pyramidOutFolder = join(outFolder)
   try {
     if (!existsSync(pyramidOutFolder)) {
       mkdirSync(pyramidOutFolder, {
@@ -83,15 +90,16 @@ const createPyramid = async (fileName, outFolder = '') => {
       }
       resizePromises.push(new Promise((resolve, reject) => {
         try {
+          const outFileName = join(pyramidOutFolder, `${imageName}_${scale}.jpg`)
           image.resize(scale)
             .jpeg({
               quality: 100
             })
-            .toFile(join(pyramidOutFolder, `${scale}.jpg`), (err) => {
+            .toFile(outFileName, (err) => {
               if (err) {
                 reject(err)
               }
-              resolve('ok')
+              resolve(outFileName)
             })
         } catch (error) {
           reject(error)
